7e68d76b432c011cd86c78457b0e6768cf6715ed
toimurhasan <2imur.hasan@gmail.com>
2025-03-25 12:04:12 +0600

    solved

--- react.js
+++ react.js
@@ -1,10 +1,16 @@
 function render(reactElement, containerDOMElement) {
-  /* Your code here! */
+  const newElement = document.createElement(reactElement.type);
+  newElement.innerText = reactElement.children;
+  for (const key in reactElement.props) {
+    newElement.setAttribute(key, reactElement.props[key]);
+  }
+  containerDOMElement.appendChild(newElement);
 }
 
 const reactElement = {
   type: "a",
   props: {
+    id: "my-id",
     href: "https://wikipedia.org/",
   },
   children: "Read more on Wikipedia",
